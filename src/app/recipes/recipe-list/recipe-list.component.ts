ea427b82e8ea0b1ccb12de03ff4ff26f8a2f8977
nitish24n <nitish24m@gmail.com>
2018-04-17 20:40:03 +0530

    adding content to recipe components

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,7 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-  recipes = []
+  recipes : Recipe[] = [
+    new Recipe('A test recipe','This is simply a test',
+    'http://chefjulieyoon.com/wp-content/uploads/2012/01/TortelliniMeatballs-20.jpg')
+  ];
+
 
   constructor() { }
 
